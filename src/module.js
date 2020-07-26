46b959e62c8c53e09518410b6f5101927c4bf959
Kumamon <Hallygirl@mail.ru>
2020-07-26 15:15:08 +0300

    'add: final project initialisation'

--- src/module.js
+++
@@ -1,7 +0,0 @@
-console.log('hkjl')
-
-async function st() {
-  await Promise.resolve()
-}
-
-st()
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,29 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production' // в каком режиме находится??
 const isDev = !isProd
+
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+const jsLoaders = () => {
+  const loaders = [{
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env']
+      }
+  }]
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+  return loaders;
+}
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: './index.js',
+  entry: ['@babel/polyfill', './index.js'],
   output: {
-    filename: 'bundle.[hash].js',
+    filename: filename('js'),
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
@@ -22,18 +36,28 @@ module.exports = {
       '@core': path.resolve(__dirname, 'src/core'),
     }
   },
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    port: 3000,
+    hot: isDev
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
-      template: 'index.html'
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd
+      }
     }),
     new CopyPlugin({
       patterns: [
-        {from: path.resolve(__dirname, 'src/favicon.png'), to: path.resolve(__dirname, 'dist')}
+        {from: path.resolve(__dirname, 'src/favicon.png'),
+          to: path.resolve(__dirname, 'dist')}
       ],
     }),
     new MiniCssExtractPlugin({
-      filename: 'bundle.[hash].css'
+      filename: filename('css')
     })
   ],
   module: {
@@ -41,7 +65,13 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true
+            }
+          },
           'css-loader',
           'sass-loader',
         ],
@@ -49,6 +79,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
+        use: jsLoaders(),
         loader: {
           loader: "babel-loader",
           options: {
